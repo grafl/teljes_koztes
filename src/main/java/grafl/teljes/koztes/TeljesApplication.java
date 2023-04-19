f79aecdf25344c5977f862cf1419e3fc7878ebcf
Laszlo Graf <laszlo.graf@msg.group>
2023-04-19 11:50:46 +0300

    Kezdeti Spring alkalmazás

---
+++ src/main/java/grafl/teljes/koztes/TeljesApplication.java
@@ -0,0 +1,13 @@
+package grafl.teljes.koztes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeljesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeljesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grafl/teljes/koztes/TeljesApplicationTests.java
@@ -0,0 +1,13 @@
+package grafl.teljes.koztes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeljesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
